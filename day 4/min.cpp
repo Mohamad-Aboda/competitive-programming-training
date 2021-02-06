15cfba0f0d64b700a1080d46f34cacc13b8203f8
Mohamad-Aboda <sirmohamedabdelhamid11@gmail.com>
2021-02-06 21:07:57 +0200

    ...

---
+++ day 4/min.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <stdio.h>
+#include <unordered_set>
+#include <iomanip>
+#include <cstring>
+#include <math.h>
+#include <queue>
+#define fast(); ios_base::sync_with_stdio(0);cin.tie(0);
+#define rep(i, n) for(int i= 0; i < (n); ++i)
+#define repp(i, n) for(int i = 1; i <= (n); ++i)
+#define ll long long
+using namespace std;
+
+
+
+// void one(){
+//     stack<int>main;
+//     stack<int>helper;
+
+//     if(main.empty()){
+//         main.push(ele);
+//         helper.push(ele);
+//     }
+//     else{
+//         if(main.top() < helper.top()){
+//             helper.push(ele);
+//         }
+
+//         if(main.pop() == helper.top()){
+//             helper.pop()
+//         }
+
+
+//         int get_min(stack<int>st){
+//             return st.top();
+//         }
+
+//     }
+// }
+
+
+// void two(){
+//     int x;cin >> x;
+//     stack<pair<int, int>>st;
+//     if(st.empty()){
+//         st.push({x, x});
+//     }else{
+//         st.push({x, st.top().second});
+//     }
+
+// }
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    // stack<pair<int, int>>stk;
+    // stk.push({7, 7});
+    // stk.push({3, 3});
+    // stk.push({2, 2});
+    // stk.push({1,1});
+    // stk.push({10, 1});
+
+    // cout << "hi" << endl;
+    // cout << stk.top().firsbt << stk.top().second << endl;
+   
+
+
+    cout << "hi" << endl;
+    return 0;
+}
+
