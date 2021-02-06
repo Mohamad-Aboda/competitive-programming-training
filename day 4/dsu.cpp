e8e6e9dcb44434799ce22afedcd75f27ce68d4a6
Mohamad-Aboda <sirmohamedabdelhamid11@gmail.com>
2021-02-06 19:55:31 +0200

    day 4

---
+++ day 4/dsu.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <stdio.h>
+#include <unordered_set>
+#include <iomanip>
+#include <cstring>
+#include <math.h>
+#include <queue>
+#define fast(); ios_base::sync_with_stdio(0);cin.tie(0);
+#define rep(i, n) for(int i= 0; i < (n); ++i)
+#define repp(i, n) for(int i = 1; i <= (n); ++i)
+#define ll long long
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t;cin >> t;
+    cout << t << endl;
+}
+
